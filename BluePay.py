ab39f47760fa1cc41ddbaef204baf69183df4d20
Bharath Ogety <BOget1@unh.newhaven.edu>
2017-11-26 19:12:58 -0500

    Deleting Test cases from the BluePay library file

--- BluePay.py
+++ BluePay.py
@@ -2,6 +2,9 @@
 # https://stackoverflow.com/questions/20557999/xor-python-text-encryption-decryption
 # https://gist.github.com/mgeeky/589b2cf781901288dfea0894a780ff98
 # https://www.devdungeon.com/content/working-binary-data-python
+"""
+BluePay library downloaded from the BluePay website:  https://github.com/jslingerland/BluePay-Sample-Code
+"""
 
 
 
@@ -787,60 +790,3 @@ class BluePay:
         # Amount associated with the transaction
         self.amount_response = self.response['amount'][0] if 'amount' in self.response else ''
 
-
-
-##
-# BluePay Python Sample code.
-#
-# This code sample runs a $3.00 CC Sale transaction
-# against a customer using test payment information.
-##
-#from __future__ import print_function
-import os.path, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-#from BluePay import BluePay
-
-account_id = "Merchant's Account ID Here"
-secret_key = "Merchant's Secret Key Here"
-mode = "TEST"  
-
-payment = BluePay(
-    account_id = account_id, 
-    secret_key = secret_key, 
-    mode = mode
-) 
-
-payment.set_customer_information(
-    name1 = "Bob",
-    name2 = "Tester",
-    addr1 = "123 Test St.",
-    addr2 = "Apt #500",
-    city = "Testville",
-    state = "IL",
-    zipcode = "54321",
-    country = "USA"
-)
-
-payment.set_cc_information(
-    card_number = "4111111111111111",
-    card_expire = "1220",
-    cvv2 = "123"    
-)
-
-payment.sale(amount = '3.00') # Sale Amount: $3.00
-
-# Makes the API Request
-payment.process()
-
-# Read response from BluePay
-if payment.is_successful_response():
-    print('Transaction Status: ' + payment.status_response)
-    print('Transaction Message: ' + payment.message_response)
-    print('Transaction ID: ' + payment.trans_id_response)
-    print('AVS Result: ' + payment.avs_code_response)
-    print('CVV2 Result: ' + payment.cvv2_code_response)
-    print('Masked Payment Account: ' + payment.masked_account_response)
-    print('Card Type: ' + payment.card_type_response)
-    print('Auth Code: ' + payment.auth_code_response)
-else:
-    print(payment.message_response)
